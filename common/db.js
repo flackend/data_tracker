4ba61a6dd622e8080a115869c01a187f2f7b7cfa
Jared Flack <jared.flack@gmail.com>
2023-05-04 09:22:11 -0400

    MySQL for prod, SQLite for dev

--- common/db.js
+++ common/db.js
@@ -1,6 +1,23 @@
-module.exports = require('knex')({
-  client: 'sqlite3',
-  connection: {
-    filename: process.env.DATABASE,
-  },
-});
+let config = {};
+
+if (process.env.ENVIRONMENT === 'development') {
+  config = {
+    client: 'sqlite3',
+    connection: {
+      filename: process.env.DB_DATABASE,
+    },
+  };
+} else if (process.env.ENVIRONMENT === 'production') {
+  config = {
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      port : 3306,
+      user : process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_DATABASE,
+    }
+  };
+}
+
+module.exports = require('knex')(config);
